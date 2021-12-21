bb04f7adce5f007a358ccc45e4dc8e42dc5e6357
mlrcloud <manuellazaror@gmail.com>
2021-12-21 13:10:01 +0100

    deploy.ps1 comments

--- deploy.ps1
+++ deploy.ps1
@@ -1,5 +1,5 @@
 # Warning: Running this script multiple times will cause the admin
-# password for the VMs to be changed.
+# password for the VMs to be changed. Comment and uncomment properly.
 
 Import-Module Tools
 
@@ -17,6 +17,6 @@ $deploymentName="Base-Deployment-$(New-Guid)"
 $params = "{ \`"vmJumpAdminPassword\`":{\`"value\`": \`"${vmJumpAdminPassword}\`" }, \`"vmAddsDnsAdminPassword\`":{\`"value\`": \`"${vmAddsDnsAdminPassword}\`" }, \`"vmSpoke1AdminPassword\`":{\`"value\`": \`"${vmSpoke1AdminPassword}\`" }}"
 
 # The deployment is applied at the subscription scope
-# TODO: Ensure the parameters.json file us up to date
-# TODO: For production deployments, update the deployment parameter file in the command below.
+# Ensure the parameters.json file is up to date
+# For production deployments, update the deployment parameter file in the command below.
 az deployment sub create -l westeurope -n $deploymentName --template-file '.\base\main.bicep' --parameters '.\parameters.json' --parameters $params
